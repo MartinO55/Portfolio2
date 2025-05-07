5b904a918a139eadc83888248b71d043a11d293f
MartinO55 <84759288+MartinO55@users.noreply.github.com>
2025-05-07 15:07:00 +1000

    animation stuff

--- scripts/main.js
+++ scripts/main.js
@@ -8,6 +8,43 @@ document.addEventListener("DOMContentLoaded", () => {
   const next = document.querySelector(".carousel__next");
   const prev = document.querySelector(".carousel__prev");
 
+  const aboutSection = document.getElementById("aboutMe__Hider");
+  const aboutTrigger = document.getElementById("aboutMe__circle");
+
+  const LineToAbout = document.getElementById("aboutMe__blurb__lineTo");
+  const LineToSkills = document.getElementById(
+    "aboutMe__SkillsGallery__LineTo"
+  );
+
+  function revealAboutSection() {
+    aboutSection.classList.remove("section__hidden");
+    aboutSection.classList.add("section__visible");
+
+    LineToAbout.style.animation = "none";
+    LineToSkills.style.animation = "none";
+
+    void LineToAbout.offsetWidth;
+    void LineToSkills.offsetWidth;
+
+    LineToAbout.style.animation = "growLineToAboutMe 0.5s ease-out forwards";
+    LineToSkills.style.animation =
+      "growLineToSkillsGallery 0.5s ease-out forwards";
+  }
+
+  window.addEventListener("scroll", () => {
+    const revealPoint = aboutSection.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    if (revealPoint < windowHeight - 100) {
+      revealAboutSection();
+    }
+  });
+
+  aboutTrigger?.addEventListener("click", () => {
+    aboutSection.scrollIntoView({ behavior: "smooth" });
+    revealAboutSection();
+  });
+
   const cardWidth = track.querySelector(".Project__Card").offsetWidth + 32; // card + gap
 
   next.addEventListener("click", () => {
