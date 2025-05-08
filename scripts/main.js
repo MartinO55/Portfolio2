c7e7cf2bad7962d0be2b56d6f96e0f7c4a8e07d2
MartinO55 <84759288+MartinO55@users.noreply.github.com>
2025-05-08 11:22:32 +1000

    js for portfolio section animation done

--- scripts/main.js
+++ scripts/main.js
@@ -10,11 +10,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const aboutSection = document.getElementById("aboutMe__Hider");
   const aboutTrigger = document.getElementById("aboutMe__circle");
+  const portfolioSection = document.getElementById("Portfolio__Hider");
+  const portfolioTrigger = document.getElementById("Projects__circle");
 
   const LineToAbout = document.getElementById("aboutMe__blurb__lineTo");
   const LineToSkills = document.getElementById(
     "aboutMe__SkillsGallery__LineTo"
   );
+  const lineToProjects = document.getElementById("Projects__Heading__lineTo");
 
   //okay, so we should probably refactor this to make it reuseable
   function revealAboutSection() {
@@ -32,6 +35,17 @@ document.addEventListener("DOMContentLoaded", () => {
       "growLineToSkillsGallery 0.5s ease-out forwards";
   }
 
+  function revealPortfolioSection() {
+    portfolioSection.classList.remove("section__hidden");
+    portfolioSection.classList.add("section__visible");
+
+    lineToProjects.style.animation = "none";
+    void lineToProjects.offsetWidth;
+
+    lineToProjects.style.animation =
+      "growLineToProjectsHeading 0.5s ease-out forwards";
+  }
+
   window.addEventListener("scroll", () => {
     const revealPoint = aboutSection.getBoundingClientRect().top;
     const windowHeight = window.innerHeight;
@@ -39,11 +53,19 @@ document.addEventListener("DOMContentLoaded", () => {
     if (revealPoint < windowHeight - 100) {
       revealAboutSection();
     }
+    if (revealPoint < windowHeight - 200) {
+      revealPortfolioSection();
+    }
   });
 
   aboutTrigger?.addEventListener("click", () => {
     aboutSection.scrollIntoView({ behavior: "smooth" });
     revealAboutSection();
+  });
+
+  portfolioTrigger?.addEventListener("click", () => {
+    portfolioSection.scrollIntoView({ behavior: "smooth" });
+    revealPortfolioSection();
   });
 
   const cardWidth = track.querySelector(".Project__Card").offsetWidth + 32; // card + gap
